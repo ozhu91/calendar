2a349ddf0824d84138061a97c8a2f1632051325f
Oleg Zhumaev <ozhu91@yandex.ru>
2020-01-23 01:11:23 +0300

    Validation class refactor

--- scripts/app.js
+++ scripts/app.js
@@ -1,103 +1,104 @@
 "use strict";
 // Будем переделавать календарь под паттерн MVC (Model-View-Controller)
 
+// Ot -> F & k in Ov | Ot -> F & !(k in Ov) |  Ot -> O & req | Ot -> O & !req | other - ERROR
+//       t === t               return               t === t      key in Ov
+//        return                                     return        t === t
+//                                                                 return
+
+// end state:
+//   return:
+//   error:
+//     type error
+//     absence req
+
 const Validator = function (templateObj) {
-  
+
   class ValidationError extends Error {
     constructor(message) {
-        super(message);
-        this.name = "ValidationError";
+      super(message);
+      this.name = "ValidationError";
     }
   }
-  
+
+  function checkType(type, value) {
+    return typeof type() === typeof value
+  }
+
   this.validator = function (validateObj) {
     for (let key in templateObj) {
-        if (templateObj[key] instanceof Object && (typeof templateObj[key] !== "function")) {
-            if (typeof validateObj[key] !== typeof (templateObj[key].type)()) {
-                throw new ValidationError(`type error - ${key}: is not ${typeof (templateObj[key].type)()}`);
-              }
-              if (templateObj[key].required) {
-                  if (!validateObj[key]) {
-                    throw new ValidationError(`type error - ${key}: is required`);
-                  }
-              }
-        } else {
-            if (typeof validateObj[key] !== typeof (templateObj[key])()) {
-                throw new ValidationError(`type error - ${key}: is not ${typeof (templateObj[key])()}`);
-              }
-        }
+      if ('required' in templateObj[key] &&
+           templateObj[key].required &&
+           !checkType(templateObj[key].type, validateObj[key]) ) {
+        throw new ValidationError(`type error - ${key}: is not ${typeof (templateObj[key].type)()}`);
+
+      } else if (!('required' in templateObj[key]) &&
+                 key in validateObj &&
+                 !checkType(templateObj[key].type, validateObj[key])) {
+        throw new ValidationError(`type error - ${key}: is not ${typeof (templateObj[key].type)()}`);
+      }
     }
   }
-  
 }
 
 const CalendarView = function (conf) {
     const {
-            adapt = true,
+        adapt = true,
             selector = null,
             day = new Date(),
             classActiveDay = "",
             lang = 'eng',
     } = conf;
-    
+
     let month = day.getMonth();
     let years = day.getFullYear();
     let firstDayOfMonth = new Date(years, month, 1).getDay();
     let lastDateOfMonth = new Date(years, month + 1, 0).getDate();
     const outTag = document.querySelector(selector);
 
-    const months = [{
-            rus: ["Январь", "Февраль", "Март",
-                "Апрель", "Май", "Июнь",
-                "Июль", "Август", "Сентябрь",
-                "Октябрь", "Ноябрь", "Декабрь"
-            ]
-        },
-        {
-            eng: ["January", "February", "March",
-                "April", "May", "June",
-                "July", "August", "September",
-                "October", "November", "December"
-            ]
-        },
-    ];
+    const monthsList = {
+      rus: ["Январь", "Февраль", "Март",
+            "Апрель", "Май", "Июнь",
+            "Июль", "Август", "Сентябрь",
+            "Октябрь", "Ноябрь", "Декабрь"
+           ],
+      eng: ["January", "February", "March",
+            "April", "May", "June",
+            "July", "August", "September",
+            "October", "November", "December"
+           ]
+    };
 
-    const weekDays = [{
+    const weekDays = {
             rus: ["Пн", "Вт", "Ср",
                 "Чт", "Пт", "Сб", "Вс"
-            ]
-        },
-        {
+            ],
             eng: ["Sn", "Mn", "Ts",
                 "Wd", "Th", "Fr", "St"
-            ]
-        }
+            ],
+        };
 
-    ];
-  
     const validator = new Validator({
-      day: {
-        type: Array,
-        required: true,
-      },
-      color: String,
-      class: {
-        type: String,
-        required: true,
-      },
-      border: Boolean,
-      strong: Boolean,
+        day: {
+            required: true,
+            type: Array
+        },
+        color: {
+            type: String,
+            required: false,
+        },
+        class: {
+            type: String
+        },
+        border: {
+            type: Boolean
+        },
+        strong: {
+            type: Boolean
+        },
     })
-    
-    this.selector = selector;
 
-    function forSelectLang(arr) {
-        for (let i = 0; i < arr.length; i++) {
-            if (String(lang) in arr[i]) {
-                return arr[i][Object.keys(arr[i])[0]];
-            }
-        }
-    }
+    this.selector = selector;
 
     /**
      * метод перевода недели на русскую раскладку пн-вс
@@ -112,12 +113,11 @@ const CalendarView = function (conf) {
     function initRender() {
         let dayOfMonth = 1;
         let calendarTable = `
-        <div class="calendar__month" data-month="${forSelectLang(months)[month]}"> 
-        <h3 class="calendar__monthHead">${forSelectLang(months)[month]} ${years}</h3>
+        <div class="calendar__month" data-month="${monthsList["eng"][month]}"> 
         <div class="calendar__row" data-head="dayOfWeek">
         `;
-        for (let i = 0; i < forSelectLang(weekDays).length; i++) {
-            calendarTable += `<span class="calendar__item" data-weekDays="${i}">${forSelectLang(weekDays)[i]}</span>`;
+        for (let i = 0; i < weekDays[lang].length; i++) {
+            calendarTable += `<span class="calendar__item" data-weekDays="${i}">${weekDays[lang][i]}</span>`;
         }
         calendarTable += `</div>`;
         for (let week = 0; week <= 5; week++) {
@@ -135,6 +135,22 @@ const CalendarView = function (conf) {
         calendarTable += `</div>`;
         outTag.innerHTML = calendarTable;
     };
+
+    this.getMonth = () => {
+        return monthsList[lang][month];
+    };
+
+    this.setMonth = (lang = lang, month = month) => {
+        return monthsList[lang][month];
+    }
+
+    this.getYear = () => {
+        return years;
+    }
+
+    this.setYear = (year = years) => {
+        return year;
+    }
 
     /**
      * метод вызова следующего месяца
@@ -210,8 +226,6 @@ const CalendarView = function (conf) {
         });
         outTag.dispatchEvent(event);
     });
-
-
 };
 
 let currentDate = new Date();
@@ -246,7 +260,7 @@ document.querySelector('#prev')
 
 const data = {
     ar: {},
-    pushAr: function (day) {
+    pushArr: function (day) {
         if (day in data.ar) {
             data.ar[day]++;
         } else {
@@ -259,12 +273,12 @@ document.querySelector(calendar1.selector).addEventListener('clickCalendar', fun
     const PARAM = 50;
     const day = target.detail;
     if (day != "emptyItem") {
-        data.pushAr(day);
+        data.pushArr(day);
         const Q_DAY = data.ar[day]
         console.log(data.ar);
         calendar1.selectDays({
             day: [target.detail],
-            class: "",
+            class: "str",
             color: `rgb(${Q_DAY*PARAM}, 0, 0)`,
             border: false,
             strong: true,
